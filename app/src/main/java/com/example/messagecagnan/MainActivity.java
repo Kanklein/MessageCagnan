83229cb972fe951f6d95ec37ef549702735ff0e1
Kanklein <108395124+Kanklein@users.noreply.github.com>
2022-07-20 16:40:04 +0800

    Milestone 3: Use implicit intents

--- app/src/main/java/com/example/messagecagnan/MainActivity.java
+++ app/src/main/java/com/example/messagecagnan/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.messagecagnan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnSendListenerMethood();
+        btnCallListenerMethod();
+        btnMapListenerMethod();
+        btnWebListenerMethod();
+
     }
+
+    private void btnWebListenerMethod() {
+        ImageButton btnWeb = findViewById(R.id.btnWeb);
+        Uri web = Uri.parse("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        Intent intent = new Intent(Intent.ACTION_VIEW,web);
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void btnMapListenerMethod() {
+        ImageButton btnMap = findViewById(R.id.btnMap);
+        Uri map = Uri.parse("geo:0,0?q=Villa+Azalea+Phase+2b+Cotcot+Liloan");
+        Intent intent = new Intent(Intent.ACTION_VIEW,map);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void btnCallListenerMethod() {
+        ImageButton btnCall = findViewById(R.id.btnCall);
+        Uri number = Uri.parse("tel: 09562912706");
+        Intent intent = new Intent(Intent.ACTION_DIAL, number);
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+    }
+
     private void btnSendListenerMethood() {
         Intent intent = new Intent(this, MessageActivity.class);
         Button btnSend = findViewById(R.id.btnSend);
